4983634600b0cf435f5bd5db58e2617a9cda7bf2
Miso <Miso@DESKTOP-74S97PJ>
2022-11-12 09:53:20 +0900

    김미소 2022-11-12 오전 - 영화 예매 진행중

--- src/main/webapp/resources/js/ticket.js
+++ src/main/webapp/resources/js/ticket.js
@@ -2,6 +2,7 @@
 
 // 영화관 - 첫번째 라디오 버튼 checked
 $('input:radio[name=cinemaName]').eq(0).attr("checked", true);
+$('input:radio[name=movieTitle]').eq(0).attr("checked", true);
 
 // 영화관 선택시 영화관, 날짜 별 상영 영화 
 function cinemaNameChoice(cinemaNo) {
@@ -20,15 +21,15 @@ function cinemaNameChoice(cinemaNo) {
 			const movie = JSON.parse(result.mList);
 			var listWrap = $('.movie-name-wrap').html('');
 			for(var m in movie) {
+				var movieNo = movie[m].movieNo;
 				var movieTitle = movie[m].movieTitle;
 				var movieBoxTag = $('<div class="movie-name-outside"></div>');
 				var movieLabelTag = $('<label for="ticket-name'+m+'" class="title-text">' + movieTitle + '</label>')
-				var movieTitleTag = $('<input type="radio" id="ticket-name'+m+'" name="movieTitle" value="" onclick="ticketMovieday()"/>');
-				$('input[name=movieTitle]').attr('value', movieTitle);
-				
+				var movieTitleTag = $('<input type="radio" id="ticket-name'+m+'" class="title-radio-button" name="movieTitle" value="" onclick="ticketMovieday('+movieNo+', '+cinemaNo+', \''+movieTitle+'\')"/>');
 				movieBoxTag.append(movieLabelTag);
 				movieBoxTag.append(movieTitleTag);
 				listWrap.append(movieBoxTag);
+				$('#ticket-name'+m).attr('value', movieTitle);
 			}
 		},
 		error : function() {
@@ -37,9 +38,26 @@ function cinemaNameChoice(cinemaNo) {
 	});
 }
 
-function ticketMovieday() {
-	var movieTitleVal = $('input[name="movieTitle"]:checked').val();
+function ticketMovieday(movieNo, cinemaNo, movieTitle) {
+	// 상단 영화 제목 출력하기
 	var divTag = $('.ticket-movie-name');
 	$(divTag).children('p').remove();
-	$(divTag).append('<p>' + movieTitleVal + '</p>');
+	$(divTag).append('<p>' + movieTitle + '</p>');
+	var testbt = $('.tmday-button').val();
+	$.ajax({
+		type : "post",
+		url : "/movieTicketTimeMovie.yh",
+		data : {
+			'cinemaNo' : cinemaNo,
+			'movieTitle' : movieTitle
+		},
+		dataType : "json",
+		success : function(result) {
+			alert("성공!");
+			console.log(result);
+		},
+		error : function() {
+			alert("실패!");
+		}
+	});
 }
